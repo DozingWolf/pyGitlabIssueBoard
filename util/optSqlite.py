c3e1934019256a68d4bcce042318b72fd10ba36a
陈圣俞 <chenshengyu1@sinopharm.com>
2023-08-08 18:10:18 +0800

    add new db operation function and db initial function

--- util/optSqlite.py
+++ util/optSqlite.py
@@ -21,7 +21,34 @@ class sqliteDB(object):
         self.__dbSchema.commit()
         self.__dbSchema.close()
 
+    def dbInitial(self)->bool:
+        __sysObjTableList = []
+        __sysObjViewList = []
+        # 循环检查表，如果不存在就初始化，并写入需要的初始化数据
+        for __tableName in __sysObjTableList:
+            if self.checkObjTable(tname=__tableName):
+                logger.debug('Table %s is exists'%(__tableName))
+            else:
+                # 这里如何初始化表，需要想想
+                pass
+
+    def checkObjTable(self,tname:str)->bool:
+        # 检查系统必须的表对象
+        selectSql = '''
+        select count(1) as count from sqlite_master where type='table' and name=?
+        '''
+        try:
+            self.__rowData = self.__dbSchema.execute(selectSql,[tname])
+            for __row in self.__rowData:
+                if __row[0] == 0:
+                    return False
+                else:
+                    return True
+        except Exception as err:
+            raise err
+
     def createObjTable(self)->bool:
+        # 创建表对象
         createSql = '''
         create table gitlab_accesstoken
         (id integer primary key autoincrement,
